d88464aab0b8973fb3eec220ff94357df7521151
Erij Osman <erij7th@gmail.com>
2024-11-19 12:15:50 -0500

    made a moving circle

--- robot.py
+++ robot.py
@@ -4,30 +4,50 @@ from random import randint
 begin_graphics()            
 finished = False
 
-def place_player(player_x = randint(0,63), player_y = randint(0,47)):
+def place_player():
     global player_circle
-    player_circle = Circle((10 * player_x + 5, 10 * player_y + 5), 5, filled=True)
+    player_circle = Circle((randint(0,630), randint(0, 470)), 5, filled=True) 
+ball = Circle((5, 5), 5)
 
-def move_player():
-    print('moving')
-    global player_x, player_y, player_circle
+ball_x = 5
+ball_y = 5
+
+while ball_x < 635:
+    ball_x += 4
+    ball_y += 3
+
+    move_to(ball, (ball_x, ball_y))
+
+    sleep(0.02)
+remove_from_screen(ball)
+
+key_text = Text("a", (320, 240), size=48)
+
+while True:
     key = update_when('key_pressed')
-    if key == 'w' and player_y < 63:
-        player_y += 1
-        move_to(player_circle, (10 * player_x, 10 * player_y))
-    elif key == 's' and player_y > 0:
-        player_y -= 1
-        move_to(player_circle, (10 * player_x, 10 * player_y))
-    elif key == 'd' and player_x < 47:
-        player_x += 1
-        move_to(player_circle, (10 * player_x, 10 * player_y))
-    elif key == 'a' and player_x > 0:
-        player_x -= 1
-        move_to(player_circle, (10 * player_x, 10 * player_y))
+    remove_from_screen(key_text)
+    key_text = Text(key, (320, 240), size=48)
+    if key == 'q':
+        break
+        #def move_player():
+  #  print('moving')    
+   # key = update_when('key_pressed')
+ #   if key == 'w' and player_y < 63:
+   #     player_y += 1
+    #    move_to(player_circle, (10 * player_x, 10 * player_y))
+  #  elif key == 's' and player_y > 0:
+     #   player_y -= 1
+      #  move_to(player_circle, (10 * player_x, 10 * player_y))
+  #  elif key == 'd' and player_x < 47:
+     #   player_x += 1
+     #   move_to(player_circle, (10 * player_x, 10 * player_y))
+   # elif key == 'a' and player_x > 0:
+     #   player_x -= 1
+      #  move_to(player_circle, (10 * player_x, 10 * player_y))
 
 place_player()
-
-while not finished:
-    move_player()
-
+c = Circle((320, 200), 5)
+move_to(c, (300, 220))
+#while not finished:
+  #  move_player()
 end_graphics()   
