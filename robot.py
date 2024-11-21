73c55a09c670dda8fd37569cc26c597c649c1afe
Erij Osman <erij7th@gmail.com>
2024-11-21 10:30:52 -0500

    added a moving circle

--- robot.py
+++ robot.py
@@ -4,50 +4,51 @@ from random import randint
 begin_graphics()            
 finished = False
 
-def place_player():
-    global player_circle
-    player_circle = Circle((randint(0,630), randint(0, 470)), 5, filled=True) 
-ball = Circle((5, 5), 5)
+player_x = 0
+player_y = 0
+player_shape = 0
 
+def place_player():
+    global player_x, player_y, player_shape
+    player_x = 55
+    player_y = randint(0, 470)
+    player_shape = Circle((player_x ,player_y) , 5, filled=True)
+
+ball = Circle((5, 5), 5)
 ball_x = 5
 ball_y = 5
-
 while ball_x < 635:
     ball_x += 4
     ball_y += 3
 
     move_to(ball, (ball_x, ball_y))
-
     sleep(0.02)
 remove_from_screen(ball)
 
-key_text = Text("a", (320, 240), size=48)
-
-while True:
+def move_player():
     key = update_when('key_pressed')
-    remove_from_screen(key_text)
-    key_text = Text(key, (320, 240), size=48)
-    if key == 'q':
-        break
-        #def move_player():
-  #  print('moving')    
-   # key = update_when('key_pressed')
- #   if key == 'w' and player_y < 63:
-   #     player_y += 1
-    #    move_to(player_circle, (10 * player_x, 10 * player_y))
-  #  elif key == 's' and player_y > 0:
-     #   player_y -= 1
-      #  move_to(player_circle, (10 * player_x, 10 * player_y))
-  #  elif key == 'd' and player_x < 47:
-     #   player_x += 1
-     #   move_to(player_circle, (10 * player_x, 10 * player_y))
-   # elif key == 'a' and player_x > 0:
-     #   player_x -= 1
-      #  move_to(player_circle, (10 * player_x, 10 * player_y))
+    if key == '6' and player_x < 63:
+        player_x += 1
+    elif key == '3':
+        if player_x < 63:
+            player_x += 1
+        if player_y > 0:
+            player_y -= 1
+    move_to(player_shape, (10 * player_x + 5, 10 * player+5))
 
+#Ftext = Text("hello", (320, 240), size=48)
+#while True:
+  #  text = update_when('key_pressed')
+   # remove_from_screen(Ftext)
+   # text = Text(text, (320, 240), size=48)
+   # if text  == 'exit':
+      #  break
+
+#test here       
 place_player()
 c = Circle((320, 200), 5)
 move_to(c, (300, 220))
+move_player()
 #while not finished:
   #  move_player()
 end_graphics()   
--- robots.py
+++ robots.py
@@ -1,18 +1,19 @@
+def place_player(player_x, player_y):
+    print(circle((player_x, player), 10))
+
+def move_player():
+    print("I'm moving...")
+    update_when('key_pressed')
 from gasp import *          # So that you can draw things
 
 begin_graphics()            # Create a graphics window
 finished = False
 
-place_player()
+place_player(300, 200)
 
 while not finished:
     move_player()
 
 end_graphics()
 
-def place_player():
-    print("Here I am!")
 
-def move_player():
-    print("I'm moving...")
-    update_when('key_pressed')
