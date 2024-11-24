2b527d4de86f0e6607cf40b21ac5ee0f3c57e714
Erij Osman <erij7th@gmail.com>
2024-11-24 16:17:53 -0500

    When two robots hit eachother, they turn to a junk

--- robot.py
+++ robot.py
@@ -4,7 +4,8 @@ from random import randint
 begin_graphics()            
 finished = False
 robots = []
-numbots = 5
+junk = []
+numbots = 10
 
 class Player: 
     pass
@@ -20,13 +21,16 @@ def place_player():
 
 def place_robots(numbots):
     global robots
+
     robots = []
-    for i in range(numbots):
+
+    while len(robots) < numbots:
         robot = Robot()
         robot.x = randint(0, 63)
         robot.y = randint(0, 47)
-        robot.shape = Box((10 * robot.x, 10 * robot.y), 10, 10, filled=True)
-        robots.append(robot)
+        if not collided(robot, robots):
+            robot.shape = Box((10 * robot.x, 10 * robot.y), 10, 10, filled=True)
+            robots.append(robot)
 
 def move_player():
     global player
@@ -71,23 +75,41 @@ def move_player():
         move_to(player.shape, (10 * player.x + 5, 10 * player.y+5))
         break
 
-def move_robot(robot):
-    if robot.x < player.x:
-        robot.x += 1
-    elif robot.x > player.x:
-        robot.x -= 1
-    if robot.y < player.y:
-        robot.y += 1
-    elif robot.y > player.y:
-        robot.y -= 1
-    move_to(robot.shape, (10 * robot.x, 10 * robot.y))
+def move_robots():
+    global junk
+    for robot in robots:
+        if robot.x < player.x:
+            robot.x += 1
+        elif robot.x > player.x:
+            robot.x -= 1
+        if robot.y < player.y:
+            robot.y += 1
+        elif robot.y > player.y:
+            robot.y -= 1
+        move_to(robot.shape, (10 * robot.x, 10 * robot.y))
+
+        crashed_robot = robot_crashed(robot)
+
+        if crashed_robot:
+            remove_from_screen(crashed_robot.shape)
+            robots.remove(crashed_robot)
+            robot.shape = Circle((10 * robot.x, 10 * robot.y), 10, color='gray')
+            junk.append(robot)
 
 def collided_with_robots(robots):
     return player.x == robot.x and player.y == robot.y
 
-def collided():
-    for robot in robots:
-        if player.x == robot.x and player.y == robot.y:
+def robot_crashed(the_bot):
+    for a_bot in robots:
+        if a_bot == the_bot:
+            return False
+        if a_bot.x == the_bot.x and a_bot.y:
+            return a_bot
+    return False
+
+def collided(thing1, list_of_things):
+    for thing2 in list_of_things:
+        if thing1.x == thing2.x and thing1.y == thing2.y:
             return True
     return False
 
@@ -95,19 +117,43 @@ def safely_place_player():
     global player
     place_player()
     
-    while collided():
+    while collided(player, robots):
         place_player()
 
     player.shape = Circle((10 * player.x + 5 ,10 * player.y + 5) , 5, filled=True)
 
 
 def check_collisions():
-    global finished
-    for robot in robots:
-        if collided_with_robots(robot): 
+    global finished, robots, junk
+    if collided(player, robots + junk): 
+            print("You've been caught!")
             finished = True
             final_text =Text( "You've been caught!", (320, 220), size = 45)
             sleep(3)
+            return
+
+    surviving_robots = []
+
+    for robot in robots:
+        if collided(robot, junk):
+            continue
+
+        jbot = robot_crashed(robot) 
+        if jbot:
+            remove_from_screen(jbot.shape)
+            jbot.shape = Box((10 * jbot.x, 10 * jbot.y), 10, 10, filled=True)
+            junk.append(jbot)
+        else:
+            surviving_robots.append(robot)
+
+        robots = []
+        for robot in surviving_robots:
+            if not collided(robot, junk):
+                robots.append(robot)
+            if not robots:
+                finished = True
+                Text("YOU WIN!", (120, 240), size=36)
+                return
 
 
 #test here 
@@ -116,8 +162,7 @@ safely_place_player()
 
 while not finished:
     move_player()
-    for robot in robots:
-        move_robot(robot)
+    move_robots()
     check_collisions()
 
 end_graphics()   
